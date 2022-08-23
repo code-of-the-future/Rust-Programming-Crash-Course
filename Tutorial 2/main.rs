8bdee21aca244cb3fd18056c3f36a1018dd0e902
Code of the Future <75938783+code-of-the-future@users.noreply.github.com>
2022-08-23 09:34:12 +0100

    Add files via upload

---
+++ Tutorial 2/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world! Code of the Future hereeeeeee!");
+}
+
+// Cargo Tools
+
+// Create a new project: cargo new project_name
+// Build a new project: cargo build 
+// Run and compile your project: cargo run
+// Check your Rust file: cargo check 
