50177ff147929e78416630ae9692bd7f7dfa543e
Code of the Future <75938783+code-of-the-future@users.noreply.github.com>
2022-08-23 09:34:57 +0100

    Add files via upload

---
+++ Tutorial 2/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world! How are you?");
+}
+
+// fn main - main function. This function is special in
+// Rust - it is automatically executed when you run
+// your programme/code. Anything inside this main function
+// will be executed. 
+
+// the go to in Rust is 4 spaces rather than a tab
+
+// println - print a line 
+
+// exclamation mark - macro 
+
+// "Hello, world!" - this is what will be printed
+
+// semi colon ; - terminates a line 
+
+// { }  - what's executed in the main function
