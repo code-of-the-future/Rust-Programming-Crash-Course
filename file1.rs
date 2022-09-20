9b3f27ad50616de4a25fd922b19bff9409c4da76
Code of the Future <75938783+code-of-the-future@users.noreply.github.com>
2022-09-20 18:52:07 +0100

    Add files via upload

---
+++ file1.rs
@@ -0,0 +1,8 @@
+// Say we want to do some coding in here...
+
+pub fn maths(){
+	// print to console
+	println!("Hello from File 1!");
+}
+
+// pub - public function - we can access this
---
+++ main.rs
@@ -0,0 +1,8 @@
+mod file1;
+// We can create a function in another file and
+// then run it in our main file
+
+fn main() {
+    file1::maths();
+    // println!("Hello, world!");
+}
